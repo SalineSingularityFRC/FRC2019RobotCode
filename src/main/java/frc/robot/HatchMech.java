a352fa946a50c7c61169d5a4c8c635f64d7b3fe2
Plottwist <brandenamstutz@gmail.com>
2019-01-14 20:16:16 -0500

    Wrote basic SOlenoid Control for The Hatch Mech.

--- src/main/java/frc/robot/HatchMech.java
+++ src/main/java/frc/robot/HatchMech.java
@@ -1,7 +1,24 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.*;
-public class HatchMech{
 
+public class HatchMech{
+  DoubleSolenoid hatchSolenoid;  
+
+    public HatchMech(int forHatch, int revHatch){
+        hatchSolenoid = new DoubleSolenoid(forHatch, revHatch);
+    }
+
+    public void setForward(){
+        hatchSolenoid.set(DoubleSolenoid.Value.kForward);
+    }
+    
+    public void setReverse(){
+        hatchSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void setOff(){
+        hatchSolenoid.set(DoubleSolenoid.Value.kOff);
+    }
+    
 }
