232d611558cbf0ccec7b0761dc2c84b55c3f0df8
Carter Harris <carter.50@hotmail.com>
2019-01-14 20:30:56 -0500

    Added some comments and stuff to HatchMech

--- src/main/java/frc/robot/HatchMech.java
+++ src/main/java/frc/robot/HatchMech.java
@@ -1,7 +1,9 @@
 package frc.robot;
 
+
+// The HatchMech class is a basic class to open/close a double solenoid. Being used to push hatch panels on our robot.
+//Written by Carter on 1/14/18
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.*;
 public class HatchMech{
     DoubleSolenoid carterSolenoid;
 
@@ -9,14 +11,17 @@ public class HatchMech{
             carterSolenoid = new DoubleSolenoid(forHatch, revHatch);
         }
 
+        //move double solenoid forward
         public void setForward() {
             carterSolenoid.set(DoubleSolenoid.Value.kForward);
         }
 
+        //move double solenoid reverse
         public void setReverse() {
             carterSolenoid.set(DoubleSolenoid.Value.kReverse);
         }
 
+        //turn of double solenoid
         public void setOff() {
             carterSolenoid.set(DoubleSolenoid.Value.kOff);
         }
