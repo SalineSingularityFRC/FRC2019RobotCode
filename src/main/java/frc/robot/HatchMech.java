f402481567fea59274075c62a1e5f8a9cc02f8e7
Carter Harris <carter.50@hotmail.com>
2019-01-14 20:15:53 -0500

    Added HatchMech stuff

--- src/main/java/frc/robot/HatchMech.java
+++ src/main/java/frc/robot/HatchMech.java
@@ -3,5 +3,21 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.*;
 public class HatchMech{
+    DoubleSolenoid carterSolenoid;
 
+         public HatchMech(int forHatch, int revHatch){
+            carterSolenoid = new DoubleSolenoid(forHatch, revHatch);
+        }
+
+        public void setForward() {
+            carterSolenoid.set(DoubleSolenoid.Value.kForward);
+        }
+
+        public void setReverse() {
+            carterSolenoid.set(DoubleSolenoid.Value.kReverse);
+        }
+
+        public void setOff() {
+            carterSolenoid.set(DoubleSolenoid.Value.kOff);
+        }
 }
