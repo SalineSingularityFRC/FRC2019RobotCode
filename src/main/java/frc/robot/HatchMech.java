5bb6c0bc20ea32f6450da130019d6f4a783091ce
Travis Crigger <criggert@salineschools.org>
2019-01-14 20:22:05 -0500

    Set up hatch mech's in and out actions of the solenoid.

--- src/main/java/frc/robot/HatchMech.java
+++ src/main/java/frc/robot/HatchMech.java
@@ -3,5 +3,23 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class HatchMech{
-    
+    DoubleSolonoid swatterSolenoid;
+
+    public HatchMech(int forwardDrive, reverseDrive){
+        swatterSolenoid = new DoubleSolenoid(forwardDrive, reverseDrive);
+    } 
+
+    public void setOut(){
+        swatterSolenoid.set(DoubleSolonoid.Value.kforward);
+    }
+
+    public void setIn(){
+        swatterSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void setOff(){
+        swatterSolenoid.set(DoubleSolenoid.Value.kOff);
+    }
 }
+
+
