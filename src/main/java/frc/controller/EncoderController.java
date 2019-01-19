73e40125aac8bbda9064673b90818f83495bc5c1
ahmedzal <ahmedzaidlia@gmail.com>
2019-01-18 20:31:53 -0500

    ytgghjfg

--- src/main/java/frc/controller/EncoderController.java
+++ src/main/java/frc/controller/EncoderController.java
@@ -5,11 +5,34 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class EncoderController extends MotorController{
 
     public EncoderController(ControllerType currentType, int portNumber){
         super(currentType, portNumber);
+        if(currentType == ControllerType.VICTOR) {
+            DriverStation.reportWarning("You tried to use VICTOR with an encoder. :(", true);
+        }
+
     }
 
+    public void resetEncoder(){
+
+        switch(currentType){
+
+            case TALON:
+                talon.getSensorCollection().setQuadraturePosition(0, 10);
+                break;
+
+           // case SPARK:
+                
+
+
+
+
+        }
+
+    }
 
 }
