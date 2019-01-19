159a8416a8d2adff96423c51271758eaa6b756ad
Cameron Tressler <camtressler@gmail.com>
2019-01-18 19:16:02 -0500

    Created MotorController.java

---
+++ src/main/java/frc/controller/MotorController.java
@@ -0,0 +1,65 @@
+package frc.controller;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class MotorController {
+
+    public enum ControllerType {
+        VICTOR,
+        TALON,
+        SPARK
+    }
+
+    ControllerType currentType;
+    int portNumber;
+
+    VictorSPX victor;
+    TalonSRX talon;
+    CANSparkMax spark;
+
+    public MotorController(ControllerType currentType, int portNumber) {
+
+
+        this.currentType = currentType;
+
+        switch(currentType) {
+
+            case VICTOR:
+                victor = new VictorSPX(portNumber);
+                break;
+
+            case TALON:
+                talon = new TalonSRX(portNumber);
+                break;
+
+            case SPARK:
+                spark = new CANSparkMax(portNumber, MotorType.kBrushless);
+                break;
+        }
+    }
+
+    public void setMotorSpeed(double speed) {
+
+        switch(currentType) {
+            case VICTOR:
+                victor.set(ControlMode.PercentOutput, speed);
+                break;
+            case TALON:
+                talon.set(ControlMode.PercentOutput, speed);
+                break;
+            case SPARK:
+                spark.set(speed);
+                break;
+        }
+
+    }
+
+
+    
+}
