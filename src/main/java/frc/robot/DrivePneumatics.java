32b8fc2e20239e5b6cc794151b4e76bdb98e58cf
ahmedzal <ahmedzaidlia@gmail.com>
2019-01-18 19:42:59 -0500

    created drive pneumatics class

--- src/main/java/frc/robot/DrivePneumatics.java
+++ src/main/java/frc/robot/DrivePneumatics.java
@@ -7,7 +7,20 @@ public class DrivePneumatics{
 
     DoubleSolenoid doubleSolenoid;
 
-    public 
-
+    public DrivePneumatics(int forwardChannel, int reverseChannel){
+        doubleSolenoid = new DoubleSolenoid(forwardChannel, reverseChannel);
+    }
+	
+	public void setForward() {
+		doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	public void setReverse() {
+		doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	public void setOff() {
+		doubleSolenoid.set(DoubleSolenoid.Value.kOff);
+	}
 
 }
